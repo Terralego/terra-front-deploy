d414aa7f1ac312d690243f68769afce3abfe9649
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-01-08 22:30:17 +0100

    WIP

--- prod/init.sh
+++ prod/init.sh
@@ -1,4 +1,8 @@
-#! /bin/bash
+#!/bin/bash
+if [[ "${NO_START-}" ]];then
+    while true;do echo "start skipped" >&2;sleep 65535;done
+    exit 0
+fi
 # AS root
 set -ex
 SCRIPTSDIR="$(dirname $(readlink -f "$0"))"
@@ -13,4 +17,65 @@ for i in $USER_DIRS;do
     chown $APP_USER:$APP_GROUP "$i"
 done
 chown -Rf root:root /etc/sudoers*
-exec gosu $APP_USER:$APP_GROUP bash -c "prod/start.sh $@"
+NPM_INSTALL=${NPM_INSTALL-}
+MODE=${1:-nginx}
+_shell() {
+    local pre=""
+    local user="$APP_USER"
+    if [[ -n $1 ]];then user=$1;shift;fi
+    local bargs="$@"
+    local NO_VIRTUALENV=${NO_VIRTUALENV-}
+    local NO_NVM=${NO_VIRTUALENV-}
+    local NVMRC=${NVMRC:-.nvmrc}
+    local NVM_PATH=${NVM_PATH:-..}
+    local NVM_PATHS=${NVMS_PATH:-${NVM_PATH}}
+    local VENV_NAME=${VENV_NAME:-venv}
+    local VENV_PATHS=${VENV_PATHS:-./$VENV_NAME ../$VENV_NAME}
+    local DOCKER_SHELL=${DOCKER_SHELL-}
+    local pre="DOCKER_SHELL=\"$DOCKER_SHELL\";touch \$HOME/.control_bash_rc;
+    if [ \"x\$DOCKER_SHELL\" = \"x\" ];then
+        if ( bash --version >/dev/null 2>&1 );then \
+            DOCKER_SHELL=\"bash\"; else DOCKER_SHELL=\"sh\";fi;
+    fi"
+    if [[ -z "$NO_NVM" ]];then
+        if [[ -n "$pre" ]];then pre=" && $pre";fi
+        pre="for i in $NVM_PATHS;do \
+        if [ -e \$i/$NVMRC ] && ( nvm --help > /dev/null );then \
+            printf \"\ncd \$i && nvm install \
+            && nvm use && cd - && break\n\">>\$HOME/.control_bash_rc; \
+        fi;done $pre"
+    fi
+    if [[ -z "$NO_VIRTUALENV" ]];then
+        if [[ -n "$pre" ]];then pre=" && $pre";fi
+        pre="for i in $VENV_PATHS;do \
+        if [ -e \$i/bin/activate ];then \
+            printf \"\n. \$i/bin/activate\n\">>\$HOME/.control_bash_rc && break;\
+        fi;done $pre"
+    fi
+    if [[ -z "$bargs" ]];then
+        bargs="$pre && if ( echo \"\$DOCKER_SHELL\" | grep -q bash );then \
+            exec bash --init-file \$HOME/.control_bash_rc -i;\
+            else . \$HOME/.control_bash_rc && exec sh -i;fi"
+    else
+        bargs="$pre && . \$HOME/.control_bash_rc && \$DOCKER_SHELL -c \"$bargs\""
+    fi
+    export TERM="$TERM"; export COLUMNS="$COLUMNS"; export LINES="$LINES"
+    exec gosu $user sh $( if [[ -z "$bargs" ]];then echo "-i";fi ) -c "$bargs"
+}
+if [[ "$MODE" = "nginx" ]];then
+    # Run nginx
+    CONF_PREFIX='FRONT_' confenvsubst.sh /etc/nginx/conf.d/default.conf.template
+    export SUPERVISORD_CONFIGS=${SUPERVISORD_CONFIGS:-/etc/supervisor.d/cron /etc/supervisor.d/nginx}
+    exec /bin/supervisor.sh
+else
+    if [ ! -e node_modules/.bin ];then NPM_INSTALL=1;fi
+    if [[ -n $NPM_INSTALL ]];then npm install;fi
+    if [[ "$MODE" = "node" ]];then
+        # Run app (eg: for dev or SSR)
+        export SUPERVISORD_NPM_ARGS="${@-}"
+        export SUPERVISORD_CONFIGS=${SUPERVISORD_CONFIGS:-/etc/supervisor.d/cron /etc/supervisor.d/npm}
+        exec /bin/supervisor.sh
+    else
+        _shell "$@"
+    fi
+fi
